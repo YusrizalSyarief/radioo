db7c32278530515688f6a12c5c2200decabe40a6
alrisandi <galihalrisandi@gmail.com>
2021-02-24 13:47:54 +0700

    audio galeri 2

--- application/views/user/galeri.php
+++ application/views/user/galeri.php
@@ -81,38 +81,36 @@
             ?>
             <div class="col-lg-7 p-0">
                 <div class="track__content nice-scroll">
-                    <div class="single_player_container">
+                <div class="single_player_container">
                         <h4><?= $f['JUDUL']; ?></h4>
-                        <div class="jp-jplayer jplayer" data-ancestor=".jp_container_1" data-url="<?= base_url(); ?>uploads/<?= $f['NAMA_FILE']; ?>"></div>
-                            <div class="jp-audio jp_container_1" role="application" aria-label="media player">
-                                <div class="jp-gui jp-interface">
-                                    <!-- Player Controls -->
-                                    <div class="player_controls_box">
-                                        <button class="jp-play player_button" tabindex="0"></button>
-                                    </div>
-                                    <!-- Progress Bar -->
-                                    <div class="player_bars">
-                                        <div class="jp-progress">
-                                            <div class="jp-seek-bar">
-                                                <div>
-                                                    <div class="jp-play-bar">
-                                                        <div class="jp-current-time" role="timer" aria-label="time">0:00
-                                                        </div>
+                        <div class="jp-jplayer jplayer" data-ancestor=".jp_container_<?=$f['ID_GALERI']; ?>" id="jp_jplayer_<?=$f['ID_GALERI']; ?>" data-url="<?= base_url('uploads/'); ?><?=$f['NAMA_FILE']; ?>">
+                        </div>
+                        <div class="jp-audio jp_container_<?=$f['ID_GALERI']; ?>" role="application" aria-label="media player">
+                            <div class="jp-gui jp-interface">
+                                <!-- Player Controls -->
+                                <div class="player_controls_box">
+                                    <button class="jp-play player_button" tabindex="0"></button>
+                                </div>
+                                <!-- Progress Bar -->
+                                <div class="player_bars">
+                                    <div class="jp-progress">
+                                        <div class="jp-seek-bar">
+                                            <div>
+                                                <div class="jp-play-bar">
+                                                    <div class="jp-current-time" role="timer" aria-label="time">0:00
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="jp-duration ml-auto" role="timer" aria-label="duration">00:00</div>
-                                        </div>
-                                        <!-- Volume Controls -->
-                                        <div class="jp-volume-controls">
-                                            <button class="jp-mute" tabindex="0">
-                                                <i class="fa fa-volume-down"></i>
-                                            </button>
-                                            <div class="jp-volume-bar">
-                                                <div class="jp-volume-bar-value" style="width: 0%;"></div>
-                                                </div>
-                                            </div>
+                                    </div>
+                                    <div class="jp-duration ml-auto" role="timer" aria-label="duration">00:00
+                                    </div>
+                                </div>
+                                <!-- Volume Controls -->
+                                <div class="jp-volume-controls">
+                                    <button class="jp-mute" tabindex="0"><i class="fa fa-volume-down"></i></button>
+                                    <div class="jp-volume-bar">
+                                        <div class="jp-volume-bar-value" style="width: 0%;">
                                         </div>
                                     </div>
                                 </div>
