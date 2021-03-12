bf2093916a4153fa4d7471b73a8866272a6df5f9
audicade <audi.pratama.45@gmail.com>
2021-03-12 21:18:46 +0700

    selalu ada yang baru

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -142,52 +142,6 @@
 </section>
 <!-- Penyiar Section -->
 
-<!-- Services Section Begin -->
-<!-- <section class="services">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-6 p-0">
-                <div class="services__left set-bg" data-setbg="img/services/service-left.jpg">
-                    <a href="https://www.youtube.com/watch?v=JGwWNGJdvx8" class="play-btn video-popup"><i class="fa fa-play"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-6 p-0">
-                <div class="row services__list">
-                    <div class="col-lg-6 p-0 order-lg-1 col-md-6 order-md-1">
-                        <div class="service__item deep-bg">
-                            <img src="<?= base_url()?>assets/user/img/services/service-1.png" alt="">
-                            <h4>Wedding</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 p-0 order-lg-2 col-md-6 order-md-2">
-                        <div class="service__item">
-                            <img src="<?= base_url()?>assets/user/img/services/service-2.png" alt="">
-                            <h4>Clubs and bar</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 p-0 order-lg-4 col-md-6 order-md-4">
-                        <div class="service__item deep-bg">
-                            <img src="<?= base_url()?>assets/user/img/services/service-4.png" alt="">
-                            <h4>DJ lessons</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 p-0 order-lg-3 col-md-6 order-md-3">
-                        <div class="service__item">
-                            <img src="<?= base_url()?>assets/user/img/services/service-3.png" alt="">
-                            <h4>Corporate events</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
-<!-- Services Section End -->
-
 <!-- Galeri Section -->
 <section class="track spad">
     <div class="container">
